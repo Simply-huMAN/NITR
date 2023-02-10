3de531cf85d506e71839a73aa4bd37f625ce251b
Sushil Kumar Katikia <sushilkatikia@gmail.com>
2023-02-10 11:28:25 +0530

    Modified q3 files
    I modified the q3 files along with the word (.odt file) and .pdf files.

---
+++ Sem 6/CN Lab/Lab 5/l5_q3server.c
@@ -0,0 +1,64 @@
+// Server side implementation of UDP client-server model
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+//#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define SA struct sockaddr
+   
+#define PORT 6969
+#define MAXLINE 1024
+
+int prime(int num){
+	for(int i=2;i*i<=num;i++){
+		if(num%i==0){
+			printf("Not a prime number\n");
+			return 0;
+		}
+	}
+	printf("Prime number\n");
+	return 1;
+}
+
+int main(){
+	int sockfd,res,num;
+	struct sockaddr_in servaddr, cliaddr;
+	
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = 6969;
+	int ser_len = sizeof(servaddr);
+	int cli_len = sizeof(cliaddr);
+	sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	bind(sockfd,(SA *)&servaddr,ser_len);
+	int diff,prod,quo;
+	while(1){
+		printf("____________________________\n");
+		printf("Ready to receive datagram :)\n");
+		recvfrom(sockfd,&res,sizeof(res),0,(SA *)&cliaddr,&cli_len);
+		recvfrom(sockfd,&num,sizeof(num),0,(SA *)&cliaddr,&cli_len);
+		printf("Received num1: %d\tnum2: %d\n",res,num);
+		diff = res-num;
+		prod = res*num;
+		//for(int i=0;i<3;i++) sans[i]=htonl(ans[i]);
+		if(num==0) quo=-1;
+		else quo = res/num;
+		printf("\nDifference: %d\nProduct: %d\nQuotient: %d\n",diff,prod,quo);
+		printf("%d: ",res);
+		res=prime(res);
+		printf("%d: ",num);
+		num=prime(num);
+		sendto(sockfd,&diff,4,0,(SA *)&cliaddr,cli_len);
+		sendto(sockfd,&prod,4,0,(SA *)&cliaddr,cli_len);
+		sendto(sockfd,&quo,4,0,(SA *)&cliaddr,cli_len);
+		sendto(sockfd,&res,4,0,(SA *)&cliaddr,cli_len);
+		sendto(sockfd,&num,4,0,(SA *)&cliaddr,cli_len);
+		printf("----------------------------\n\n");
+	}
+	close(sockfd);
+	return 0;
+}
